dbdc35c1454f41235396537cb7d9d5ac64ce118e
pdruart <pdruart@student.codam.nl>
2021-05-06 17:58:20 +0200

    weird undefined behavior for native printf when providing flags/width/etc. for the percent specifier

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 
 int	main(void)
 {
-	   printf("%%yes %08.3dc a %-8.3d b %8d", -13, -12, -11);
+	   printf("\%0*.5%syes %08.3dc a %-8.3d b %8d", 4, -13, -12, -11);
 	   printf("\n");
-	ft_printf("%%yes %08.3dc a %-8.3d b %8d", -13, -12, -11);
+	ft_printf("\%0*.5%syes %08.3dc a %-8.3d b %8d", 4, -13, -12, -11);
 }
