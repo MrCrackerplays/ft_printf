5a682749cb8c1ed0a242ba254027d49b88128a74
Patrick Druart <pdruart@student.codam.nl>
2021-09-01 14:56:42 +0200

    added headers to comply with updated norm

--- parse_specifier_i.c
+++ parse_specifier_i.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   parse_specifier_i.c                                :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: pdruart <pdruart@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2021/09/01 14:55:02 by pdruart       #+#    #+#                 */
+/*   Updated: 2021/09/01 14:55:02 by pdruart       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "ft_printf.h"
 
 char	*parse_specifier_i(va_list *arg, t_conv *data)
